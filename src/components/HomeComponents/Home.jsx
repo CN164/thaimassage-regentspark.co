7257a8ec6895fa41242d04d6c7ab50960fb8e130
Purin Tadsorn <purintadsorn@gmail.com>
2021-11-09 16:55:59 +0700

    update-menu-z-index-new-imgae-fade

--- src/components/HomeComponents/Home.jsx
+++ src/components/HomeComponents/Home.jsx
@@ -4,7 +4,7 @@ import { Fade } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 
 const fadeImages = [
-    "/banner_1.jpg","https://images.unsplash.com/photo-1600334129128-685c5582fd35?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80","https://images.unsplash.com/photo-1544161515-4ab6ce6db874?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80"
+    "/banner_1.jpg","https://images.unsplash.com/photo-1600334129128-685c5582fd35?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80","https://images.unsplash.com/photo-1544161515-4ab6ce6db874?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80","/s1.jpg","/s2.jpg","/s3.jpg","/s4.jpg"
 ];
 
 function Home() {
@@ -20,6 +20,18 @@ function Home() {
                     </div>
                     <div className="each-fade">
                         <img src={fadeImages[2]} />
+                    </div>
+                    <div className="each-fade">
+                        <img src={fadeImages[3]} />
+                    </div>
+                    <div className="each-fade">
+                        <img src={fadeImages[4]} />
+                    </div>
+                    <div className="each-fade">
+                        <img src={fadeImages[5]} />
+                    </div>
+                    <div className="each-fade">
+                        <img src={fadeImages[6]} />
                     </div>
                 </Fade>
             </div>
